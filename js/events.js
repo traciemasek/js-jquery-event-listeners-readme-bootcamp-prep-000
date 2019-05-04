fd03326826d8cd14d5a8e63a60888f21262f2f71
Tracie Masek <traciemasek@gmail.com>
2019-05-04 19:48:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,8 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', function() {
-    if(keydown.value)
+  $('input').on('keydown', function(key) {
+    if(key.which === 
   })
 }
 
